24267ac3399d3e9e1a9a525df9e0a7a105684bf4
Rilakkumari <kubotamarlene@gmail.com>
2021-08-02 15:51:52 +0900

    added API resulted in daily forecast not showing up. Deleted again.
    Repeat Lesson 5, week  8.

--- src/app.js
+++ src/app.js
@@ -42,8 +42,7 @@ let form = document.querySelector("#change-city-form");
 form.addEventListener("submit", search);
 
 //Temperature, icons and extra info
-function displayForecast(response) {
-  console.log(response.data.daily);
+function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -67,14 +66,6 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coordinates) {
-  let apiKey = "663df824629c10b5cb37f18468e84501";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
-}
-
-
 function showTemperature(response) {
   let city = document.querySelector("h2");
   let localTemperature = document.querySelector("#currenttemp");
@@ -92,8 +83,6 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-
-  getForecast(response.data.coord);
 }
 
 //Search function
@@ -136,6 +125,8 @@ let cLink = document.querySelector(".celsius-link");
 cLink.addEventListener("click", cConvert);
 
 let celsiusTemperature = null;
+
+displayForecast();
 
 //function changeCitiesParis(event) {
 //event.preventDefault();
